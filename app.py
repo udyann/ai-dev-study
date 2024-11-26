627e563a7ed1a5d82e23a9392380b45fd329a22f
udyann <udyann@Udyann.>
2024-11-26 10:35:16 +0900

    week-6

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from fastapi import FastAPI
 from pydantic import BaseModel
 from model.model import SentimentModel
@@ -7,6 +8,22 @@ model = SentimentModel()
 
 class TextInput(BaseModel):
     text: str
+
+@app.get("/")
+def root():
+    return {"message" : "Hello World"}
+
+@app.get("/home")
+def home():
+    return {"message" : "home"}
+
+@app.get("/home/{name}")
+def read_name(name: str):
+    return {"name" : name}
+
+@app.post("/")
+def home_posst(msg: str):
+    return {"Hello" : "POST", "msg" : msg}
 
 @app.post("/predict")
 async def predict_sentiment(input: TextInput):
--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 from enum import Enum
 from fastapi import FastAPI
 from pydantic import BaseModel
-from pydantic import Field
-
-class ModelName(str, Enum):
-	alexnet = "alexnet"
-	resnet = "resnet"
-	lenet = "lenet"
-
-class DataInput(BaseModel):
-	name: str
+from model.model import SentimentModel
 
 app = FastAPI()
+model = SentimentModel()
+
+class TextInput(BaseModel):
+    text: str
 
 @app.get("/")
-async def root():
-	return {"message" : "Hello World"}
+def root():
+    return {"message" : "Hello World"}
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-	if model_name is ModelName.alexnet:
-		return {"model_name" : model_name, "message" : "Deep Learning FTW!"}
-	
-	if model_name.value == "lenet":
-		return {"model_name" : model_name, "message" : "LeCNN all the images"}
+@app.get("/home")
+def home():
+    return {"message" : "home"}
 
-	return {"model_name" : model_name, "message": "Have some residuals"}
+@app.get("/home/{name}")
+def read_name(name: str):
+    return {"name" : name}
 
 @app.post("/")
-def home_post(data_request: DataInput):
-	return {"Hello": "POST", "msg": data_request.name}
+def home_posst(msg: str):
+    return {"Hello" : "POST", "msg" : msg}
+
+@app.post("/predict")
+async def predict_sentiment(input: TextInput):
+    prediction = model.predict(input.text)
+    return {"text": input.text, "sentiment": prediction}
